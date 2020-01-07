daaf8538fead29c1d4d929211cc97cb65f4f1555
rphanley <rphanley@rocketmail.com>
2020-01-07 18:17:01 +0000

    Corrected settings.py

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -88,8 +88,7 @@ if development:
         }
     }
 else:
-    
-DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
+    DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
 
 
 # Password validation
